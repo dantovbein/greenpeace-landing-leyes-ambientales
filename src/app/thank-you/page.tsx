73d5f9cd661a66a5ec3e2c61d0bebc7fc484d0f9
Dan Tovbein <dtovbein@gmail.com>
2024-01-11 09:53:41 -0300

    ,

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -4,21 +4,19 @@ import { FC } from "react";
 import ThankYou from '@/app/_components/ThankYou'
 // import Stats from '@/app/_components/Stats'
 
-// const Page:FC<{}> = () => {
-//   return (
-//     <>
-//       <ThankYou>
-//         <Stats />
-//       </ThankYou>
-//     </>
-//   )
-// }
 const Page:FC<{}> = () => {
   return (
     <>
-      Muchas gracias por firmar.
+      <ThankYou />
     </>
   )
 }
+// const Page:FC<{}> = () => {
+//   return (
+//     <>
+//       Muchas gracias por firmar. 
+//     </>
+//   )
+// }
 
 export default Page;
